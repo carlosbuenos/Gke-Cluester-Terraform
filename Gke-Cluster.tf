537db49319ce10b63a72c7afbb8909619fa13c0d
Carlos Roberto Cardoso Dos Reis Bueno <ccarlosbueno@outlook.com>
2020-01-05 07:03:58 -0300

    Documento Terraform

---
+++ Gke-Cluster.tf
@@ -0,0 +1,8 @@
+resource "google_container_cluster" "clbueno-1" {
+  name               = "clbueno-1"
+  network            = "default"
+  location               = "us-central1-a"
+  initial_node_count = 4
+  enable_kubernetes_alpha = "true"
+  enable_legacy_abac = "true"
+}
